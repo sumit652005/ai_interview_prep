e6071a10d5bdf6d2b35592a5c788802cb22a2847
Sumit Das <sumitdas7864865@gmail.com>
2025-04-24 13:28:24 +0530

    implement interview session

--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -5,7 +5,7 @@ import { Button } from './ui/button';
 import Link from 'next/link';
 import DisplayTechIcons from './DisplayTechIcons';
 
-const InterviewCard = ({ interviewId, userId, role, type, techstack, createdAt }: InterviewCardProps) => {
+const InterviewCard = ({ id, userId, role, type, techstack, createdAt }: InterviewCardProps) => {
 
     const feedback = null as Feedback | null;
     const normalizedType = /mix/gi.test(type) ? 'Mixed' : type;
@@ -47,8 +47,8 @@ const InterviewCard = ({ interviewId, userId, role, type, techstack, createdAt }
                 
                 <Button className="btn-primary">
                     <Link href={feedback 
-                        ? `/interview/${interviewId}/feedback`
-                        : `/interview/${interviewId}`
+                        ? `/interview/${id}/feedback`
+                        : `/interview/${id}`
                     }>
                         {feedback ? 'Check Feedback' : 'View Interview'}
                     </Link>
